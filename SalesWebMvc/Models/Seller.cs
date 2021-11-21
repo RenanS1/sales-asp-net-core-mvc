d0bc308a040d7c0a7f83c9db6354c676a18c67ca
Renan Silva <renans100.social@outlook.com>
2021-11-21 16:49:54 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime Date { get; set; }
+        public double BaseSalary { get; set; }
+        public Department department { get; set; }
+        public List<SalesRecord> SalesRecord { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        { 
+        }
+
+        public Seller(int id, string name, string email, DateTime date, double baseSalary)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Date = date;
+            BaseSalary = baseSalary;
+        }
+
+        public void AddSales(SalesRecord salesRecord)
+        {
+            SalesRecord.Add(salesRecord);
+        }
+
+        public void RemoveSales(SalesRecord salesRecord)
+        {
+            SalesRecord.Remove(salesRecord);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return SalesRecord.Where(sr => sr.Date >= initial && sr.Date <= final).Sum(sr => sr.Amount);
+        }
+    }
+}
